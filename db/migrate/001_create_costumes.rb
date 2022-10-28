16e9b3c64498a43e8c604841f60075941775e767
Victoria Parry <vlparry@gmail.com>
2022-10-28 14:08:28 -0400

    commit tree b0fbaa0983ff8c3604ddf024f0f0e731b1055ac8

---
+++ db/migrate/001_create_costumes.rb
@@ -0,0 +1,18 @@
+# The costumes table will have four columns:
+
+# name (string)
+# price (float)
+# size (string)
+# image_url (string)
+
+class CreateCostumes < ActiveRecord::Migration[6.1]
+    def change
+      create_table :costumes do |t|
+        t.string :name
+        t.float :price
+        t.string :size
+        t.string :image_url
+        t.timestamps
+      end
+    end
+  end
